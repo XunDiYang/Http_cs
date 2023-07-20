5992016aee9442dda846c0ef7b6e87dd7e16f2e6
XunDiYang <1920310178@qq.com>
2023-07-20 17:53:03 +0800

    Create HttpRequest.java

---
+++ app/src/main/java/com/socket/http_cs/model/HttpRequest.java
@@ -0,0 +1,36 @@
+package com.socket.http_server_client.model;
+
+import java.util.Map;
+
+public class HttpRequest {
+    public static class StartLine{
+        HTTP_METHOD method;
+        String uri;
+        int port;
+        HTTP_VERSION version;
+
+        public StartLine(HTTP_METHOD method, String uri, int port, HTTP_VERSION version) {
+            this.method = method;
+            this.uri = uri;
+            this.port = port;
+            this.version = version;
+        }
+    }
+
+    private StartLine startLine;
+    private Map<String,String> headers;
+    private String messageBody;
+
+    public HttpRequest() {
+        this.startLine = null;
+        this.headers = null;
+        this.messageBody = null;
+    }
+
+    public HttpRequest(StartLine startLine, Map<String, String> headers, String messageBody) {
+        this.startLine = startLine;
+        this.headers = headers;
+        this.messageBody = messageBody;
+    }
+
+}
