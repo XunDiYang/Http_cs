3c76ed8bb24e7e0d50f208526ce17b005001ca26
XunDiYang <1920310178@qq.com>
2023-07-20 17:54:24 +0800

    Update TcpServer.java

--- app/src/main/java/com/socket/http_cs/server/service/TcpServer.java
+++ app/src/main/java/com/socket/http_cs/server/service/TcpServer.java
@@ -1,10 +1,8 @@
-package com.socket.http_cs.server.service;
+package com.socket.http_server_client.server.service;
 
-
+import android.os.Handler;
 import android.util.Log;
-
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.ServerSocket;
@@ -12,31 +10,37 @@ import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class TcpServer {
-    private final String TAG = "TcpServer";
-    protected final String serverIp;
-    protected final int serverPort;
-    protected final ExecutorService mExecutorService;
-    protected ServerSocket serverSocket;
-    protected volatile boolean runServer = true;
+public abstract class TcpServer {
+    private final String TAG = "LongTcpServer";
+    protected  String serverIp;
+    protected  int mPort;
+    protected  ExecutorService mExecutorService;
+    protected  ServerSocket serverSocket;
+    protected static volatile boolean runServer = true;
+    protected String httpServerClassPath;
     protected String handleClientClassPath;
 
-    public TcpServer(String serverIp, int port,String handleClientClassPath) {
+    public Handler handler;
+
+    public TcpServer(){
+
+    }
+
+    public TcpServer(String serverIp, int port, String httpServerClassPath, String handleClientClassPath) {
         this.serverIp = serverIp;
-        serverPort = port;
-        this.handleClientClassPath = handleClientClassPath;
+        mPort = port;
+        this.httpServerClassPath =  httpServerClassPath;
+        this.handleClientClassPath =  handleClientClassPath;
+
         mExecutorService = Executors.newFixedThreadPool(5);
         try {
-            serverSocket = new ServerSocket(serverPort);
-            Log.d(TAG, "服务器启动成功");
+            serverSocket = new ServerSocket(mPort);
+            Log.i(TAG, "服务器启动成功");
         } catch (IOException e) {
+            Log.e(TAG, "服务器启动失败" + e.getMessage());
             Log.e(TAG, "服务器启动失败" + e.getMessage());
             throw new RuntimeException(e);
         }
-    }
-
-    public void start() {
-        mExecutorService.execute(new ConnWatchDog());
     }
 
     public void stop() throws IOException {
@@ -45,43 +49,50 @@ public class TcpServer {
         serverSocket.close();
     }
 
-    class ConnWatchDog implements Runnable {
+    public void start(){
+        mExecutorService.execute(new ConnWatchDog());
+    }
+
+    public class ConnWatchDog implements Runnable {
         @Override
-        public void run() {
+        public void run(){
             while (runServer) {
                 try {
                     Socket client = serverSocket.accept();
                     client.setKeepAlive(true);
-                    Log.d(TAG, "有客户端请求链接");
+                    Log.i(TAG, "有客户端请求链接");
+                    Class c = Class.forName(httpServerClassPath);
                     Class clz = Class.forName(handleClientClassPath);
-                    Constructor handleClientConstructor = clz.getConstructor();
-                    Object handleClient = handleClientConstructor.newInstance();
-                    Method iniMethod = clz.getMethod("init",Socket.class);
-                    iniMethod.invoke(handleClient,client);
+                    Object handleClient = clz.getDeclaredConstructors()[0].newInstance(c.newInstance());
+                    Method iniClientSocket = clz.getDeclaredMethod("initClientSocket",Socket.class);
+                    iniClientSocket.invoke(handleClient,client);
                     mExecutorService.submit((Runnable) handleClient);
                 } catch (IOException e) {
                     Log.e(TAG, "有客户端链接失败" + e.getMessage());
-                } catch (ClassNotFoundException e) {
-                    throw new RuntimeException(e);
-                } catch (NoSuchMethodException e) {
+                }  catch (InstantiationException e) {
                     throw new RuntimeException(e);
                 } catch (InvocationTargetException e) {
                     throw new RuntimeException(e);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException(e);
-                } catch (InstantiationException e) {
+                } catch (NoSuchMethodException e) {
                     throw new RuntimeException(e);
                 }
             }
         }
     }
 
-    public interface HandleClient extends Runnable {
-        public void init(Socket s);
+    public static abstract class  HandleClient implements Runnable {
         @Override
-        public void run();
-        public void closeClientSocket();
+        public abstract void run();
+
+        public abstract void initClientSocket(Socket s);
+
+        public abstract void closeClientSocket();
     }
+
 }
 
 
