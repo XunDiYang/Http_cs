763a59589b28e0d1730943989b99a15a314379c6
XunDiYang <1920310178@qq.com>
2023-07-20 17:51:04 +0800

    Update LongLiveSocket.java

--- app/src/main/java/com/socket/http_cs/client/service/LongLiveSocket.java
+++ app/src/main/java/com/socket/http_cs/client/service/LongLiveSocket.java
@@ -1,60 +1,51 @@
-package com.socket.http_cs.client.service;
+package com.socket.http_server_client.client.service;
 
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
+import android.os.StrictMode;
 import android.util.Log;
 
-import com.socket.http_cs.client.callback.ReadingCallback;
-import com.socket.http_cs.client.callback.ErrorCallback;
-import com.socket.http_cs.client.callback.WritingCallback;
-import com.socket.http_cs.model.CMessage;
-import com.socket.http_cs.model.MsgType;
+import com.socket.http_server_client.client.callback.ErrorCallback;
+import com.socket.http_server_client.client.callback.ReadingCallback;
+import com.socket.http_server_client.client.callback.WritingCallback;
+import com.socket.http_server_client.model.CMessage;
+import com.socket.http_server_client.model.MsgType;
 
 import org.json.JSONException;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public final class LongLiveSocket extends Thread {
-  private static final String TAG = "LongLiveSocket";
+    private static final String TAG = "LongLiveSocket";
     private final String localIp;
-    private final String mHost;
-    private final int mPort;
+    private final String serverIp;
+    private final int serverPort;
     private final ReadingCallback mReadingCallback;
     private final ErrorCallback mErrorCallback;
     private final Handler mUIHandler = new Handler(Looper.getMainLooper());
     private HandlerThread mWriterThread;
     private Handler mWriterHandler;
     private Thread mReaderThread;
-    //    private Thread mHeartBeatThread;
-    private static Socket mSocket;  // guarded by mLock
-//    private CMessage heartBeatMsg;
-
-    private long lastSendTime; //最后一次发送数据的时间
+    private static Socket mSocket;
 
     private volatile boolean running = false; //连接状态
+    private volatile boolean runningWrite = false; //写状态
 
     public LongLiveSocket(String localIp, String host, int port,
                           ReadingCallback readingCallback, ErrorCallback errorCallback) {
         this.localIp = localIp;
-        mHost = host;
-        mPort = port;
-//        heartBeatMsg = new CMessage(localIp, mHost, 200, MsgType.PING, "");
+        serverIp = host;
+        serverPort = port;
         mReadingCallback = readingCallback;
         mErrorCallback = errorCallback;
     }
 
-    /********************************************************/
-    /*
-     *   以下使用原生Thread实现
-     *
-     *
-     * */
     @Override
     public void run() {
         iniSocket();
@@ -63,15 +54,15 @@ public final class LongLiveSocket extends Thread {
     private void iniSocket() {
         if (running) return;
         try {
-            mSocket = new Socket(mHost, mPort);
+            mSocket = new Socket(serverIp, serverPort);
             mSocket.setKeepAlive(true);
             running = true;
+            runningWrite = true;
             mWriterThread = new HandlerThread("socket-writer");
             mWriterThread.start();
             mWriterHandler = new Handler(mWriterThread.getLooper());
             mReaderThread = new Thread(new ReaderTask(mSocket), "socket-reader");
             mReaderThread.start();
-//            mHeartBeatThread = new Thread(mHeartBeatTask, "socket-heartbeat").start();
             mUIHandler.post(() -> mReadingCallback.onData(new CMessage("", "", 100, MsgType.CONNECT, "")));
         } catch (IOException e) {
             Log.e(TAG, "initSocket: ", e);
@@ -81,98 +72,98 @@ public final class LongLiveSocket extends Thread {
 
     @Override
     public void interrupt() {
-        super.interrupt();
         try {
             if (mSocket != null) {
-                Log.d(TAG, "关闭客户端的socket");
-                if( mWriterThread != null) {
+                sendEndPkg();
+                if (mWriterThread != null) {
                     mWriterThread.interrupt();
                 }
-                if(mReaderThread!=null) {
+                if (mReaderThread != null) {
                     mReaderThread.interrupt();
                 }
-//                mHeartBeatThread.interrupt();
                 mSocket.close();
+                Log.i(TAG, "关闭客户端的socket");
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+        super.interrupt();
+    }
+
+    public void sendEndPkg() {
+        if (runningWrite) {
+            if (running) {
+                int SDK_INT = android.os.Build.VERSION.SDK_INT;
+                if (SDK_INT > 8) {
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                    //your codes here
+                    WriterTask endPkg = new WriterTask(new CMessage(localIp, serverIp, 400, MsgType.CONNECT, ""), new WritingCallback() {
+                        @Override
+                        public void onSuccess() {
+                        }
+
+                        @Override
+                        public void onFail(Object cMessage) {
+                        }
+                    });
+                    endPkg.run();
+                }
+            }
+            runningWrite = false;
         }
     }
 
     public void stopServerSocket() {
         if (running) {
+            sendEndPkg();
             running = false;
         }
         mUIHandler.post(() -> mErrorCallback.onError());
     }
 
-    public void write(CMessage cMessage, WritingCallback callback) {
+    public void write(Object cMessage, WritingCallback callback) {
         mWriterHandler.post(new WriterTask(cMessage, callback));
     }
 
     private class WriterTask implements Runnable {
-        private CMessage cMsg;
+        private Object cMsg;
         private WritingCallback callback;
 
-        public WriterTask(CMessage cmsg, WritingCallback callback) {
+        public WriterTask(Object cmsg, WritingCallback callback) {
             this.cMsg = cmsg;
             this.callback = callback;
         }
 
         @Override
         public void run() {
+            if (!runningWrite) {
+                return;
+            }
             if (!running) {
                 iniSocket();
             }
             try {
-                ObjectOutputStream oos = new ObjectOutputStream(mSocket.getOutputStream());
-                oos.writeObject(cMsg);
-                Log.d(TAG, "发送：\t" + cMsg.toJsonStr());
-                oos.flush();
-                lastSendTime = System.currentTimeMillis();
+                OutputStream outToServer = mSocket.getOutputStream();
+                DataOutputStream out = new DataOutputStream(outToServer);
+                if (cMsg != null) {
+                    out.writeUTF(((CMessage) cMsg).getFrom());
+                    out.writeUTF(((CMessage) cMsg).getTo());
+                    out.writeInt(((CMessage) cMsg).getCode());
+                    out.writeInt(((CMessage) cMsg).getType());
+                    out.writeUTF(((CMessage) cMsg).getMsg());
+                    out.flush();
+                    Log.i(TAG, "发送：\t" + ((CMessage) cMsg).toJsonStr());
+                }
                 callback.onSuccess();
             } catch (IOException e) {
                 Log.e(TAG, "write: ", e);
+                runningWrite = false;  //服务器端断开了
                 callback.onFail(cMsg);
                 stopServerSocket();
             }
         }
     }
-
-//    private final HeartBeatTask mHeartBeatTask = new HeartBeatTask();
-//
-//    private class HeartBeatTask implements Runnable {
-//        long checkDelay = 10;
-//        long keepAliveDelay = 1000;
-//
-//        @Override
-//        public void run() {
-//            while (running) {
-//                if (System.currentTimeMillis() - lastSendTime > keepAliveDelay) {
-//                    write(heartBeatMsg, new WritingCallback() {
-//                        @Override
-//                        public void onSuccess() {
-//                            // 每隔 HEART_BEAT_INTERVAL_MILLIS 发送一次
-//                            Log.d(TAG,"心跳包发送成功");
-//                        }
-//                        @Override
-//                        public void onFail(CMessage cMessage) {
-//                            Log.w(TAG,"心跳包发送失败");
-//                            stopServerSocket();
-//                        }
-//                    });
-//                } else {
-//                    try {
-//                        Thread.sleep(checkDelay);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                        Log.w(TAG,"睡眠失败？");
-//                        stopServerSocket();
-//                    }
-//                }
-//            }
-//        }
-//    }
 
     private class ReaderTask implements Runnable {
 
@@ -196,15 +187,20 @@ public final class LongLiveSocket extends Thread {
         }
 
         private void readResponse() throws IOException, JSONException, ClassNotFoundException, InterruptedException {
-            InputStream inputStream = mSocket.getInputStream();
-            DataInputStream in = new DataInputStream(inputStream);
 
             while (running) {
+                InputStream inputStream = mSocket.getInputStream();
+                DataInputStream in = new DataInputStream(inputStream);
+
                 if (in.available() > 0) {
-                    ObjectInputStream ois = new ObjectInputStream(in);
-                    Object obj = ois.readObject();
-                    Log.d(TAG, "接收：\t" + obj);
-                    CMessage cMessage = (CMessage) obj;
+                    Log.i(TAG, "收到消息");
+                    CMessage cMessage = new CMessage();
+                    cMessage.setFrom(in.readUTF());
+                    cMessage.setTo(in.readUTF());
+                    cMessage.setCode(in.readInt());
+                    cMessage.setType(in.readInt());
+                    cMessage.setMsg(in.readUTF());
+                    cMessage.setMsg("来自" + cMessage.getFrom() + "客户端: " + cMessage.getMsg());
                     if (cMessage.getCode() != 200 || cMessage.getType() == MsgType.TEXT) {
                         mUIHandler.post(() -> {
                             mReadingCallback.onData(cMessage);
@@ -216,8 +212,4 @@ public final class LongLiveSocket extends Thread {
             }
         }
     }
-
-
 }
-
-
