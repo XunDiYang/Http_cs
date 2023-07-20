efd8d575e74113d3454ccae97e959584497fb5d7
XunDiYang <1920310178@qq.com>
2023-07-20 17:53:45 +0800

    Create HttpResponse.java

---
+++ app/src/main/java/com/socket/http_cs/model/HttpResponse.java
@@ -0,0 +1,34 @@
+package com.socket.http_server_client.model;
+
+import java.util.Map;
+
+public class HttpResponse {
+    /**
+     * 请求方法 GET/POST/PUT/DELETE/OPTION...
+     */
+    private String method;
+    /**
+     * 请求的uri
+     */
+    private String uri;
+    /**
+     * http版本
+     */
+    private String version;
+
+    /**
+     * 请求头
+     */
+    private Map<String, String> headers;
+
+    /**
+     * 请求参数相关
+     */
+    private String message;
+
+    public HttpResponse(String method, String uri, String version) {
+        this.method = method;
+        this.uri = uri;
+        this.version = version;
+    }
+}
